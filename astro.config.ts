f13a3c3e4e8a3ae935d243cb7ff52f471f016321
Baladithya Balamurugan <bbaladithya@gmail.com>
2023-12-14 20:06:16 -0800

    corrected paths

--- astro.config.ts
+++ astro.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config'
 import cloudflare from '@astrojs/cloudflare';
 import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
+import path from 'path';
 
 // https://astro.build/config
 export default defineConfig({
@@ -18,4 +19,9 @@ export default defineConfig({
       applyBaseStyles: false,
     }),
   ],
+  resolve: {
+    alias: {
+      "@/*": path.resolve(__dirname, "./src"),
+    }
+  }
 })
