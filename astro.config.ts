59fc9a82aca5216dfda910b6d7773688a3895665
Baladithya Balamurugan <bbaladithya@gmail.com>
2023-12-14 20:25:17 -0800

    anotha try

--- astro.config.ts
+++ astro.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config'
 import cloudflare from '@astrojs/cloudflare';
 import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
-import path from 'path';
 
 // https://astro.build/config
 export default defineConfig({
@@ -19,9 +18,4 @@ export default defineConfig({
       applyBaseStyles: false,
     }),
   ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    }
-  }
 })
