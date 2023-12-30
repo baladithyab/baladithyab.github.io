7e2adacb4dd6b9ccc1444cf24a55a2a171432b18
Baladithya Balamurugan <bbaladithya@gmail.com>
2023-12-30 13:50:47 -0800

    there was an error in HeadSEO for local testing + formatting

--- astro.config.ts
+++ astro.config.ts
@@ -1,17 +1,12 @@
 import { defineConfig } from 'astro/config'
-import cloudflare from '@astrojs/cloudflare';
+import cloudflare from '@astrojs/cloudflare'
 import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: cloudflare({
-    runtime: {
-      mode: 'local',
-      type: 'pages',
-    }
-  }),
+  adapter: cloudflare({ mode: 'directory' }),
   integrations: [
     react(),
     tailwind({
