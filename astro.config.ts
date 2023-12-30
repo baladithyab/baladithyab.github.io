84e436d6779140874897c4d44e27b81cdf659ac0
Baladithya Balamurugan <bbaladithya@gmail.com>
2023-12-30 13:58:03 -0800

    update config to use pages only not pages functions

--- astro.config.ts
+++ astro.config.ts
@@ -1,12 +1,18 @@
 import { defineConfig } from 'astro/config'
-import cloudflare from '@astrojs/cloudflare'
+import cloudflare from '@astrojs/cloudflare';
 import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: cloudflare({ mode: 'directory' }),
+  adapter: cloudflare({
+    runtime: {
+      mode: 'local',
+      type: 'pages',
+    }
+  }),
+  // adapter: cloudflare({ mode: "directory" }),
   integrations: [
     react(),
     tailwind({
