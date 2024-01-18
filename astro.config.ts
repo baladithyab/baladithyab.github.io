81d038acdab2dc346eee6f187921ccb3233f056f
bbaladithya <bbaladithya@gmail.com>
2024-01-18 07:48:17 +0000

    added auth and minification integration, testing on cloudflare

--- astro.config.ts
+++ astro.config.ts
@@ -2,22 +2,36 @@ import { defineConfig } from 'astro/config'
 import cloudflare from '@astrojs/cloudflare'
 import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
+import sentry from '@sentry/astro'
+import spotlightjs from '@spotlightjs/astro'
+import auth from 'auth-astro'
+import compress from 'astro-compress'
+const isDev = import.meta.env.DEV
+const devInteg = isDev ? [sentry(), spotlightjs()] : []
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   site: 'https://codeseys.io/',
-  // adapter: cloudflare({  // just pages
-  //   runtime: {
-  //     mode: 'local',
-  //     type: 'pages',
-  //   }
-  // }),
-  adapter: cloudflare({ mode: 'directory' }), // pages functions
+  adapter: cloudflare({
+    mode: 'directory',
+  }),
+  // pages functions
   integrations: [
+    ...devInteg,
     react(),
     tailwind({
       applyBaseStyles: false,
+      
     }),
+    auth(),
+    compress(),
   ],
 })
+
+// adapter: cloudflare({  // just pages
+//   runtime: {
+//     mode: 'local',
+//     type: 'pages',
+//   }
+// }),
