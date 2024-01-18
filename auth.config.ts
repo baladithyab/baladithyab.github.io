a8d42b0a931591016df6a7ac365df0904b457265
bbaladithya <bbaladithya@gmail.com>
2024-01-18 08:29:02 +0000

    added simple auth and removed previous workaround

---
+++ auth.config.ts
@@ -0,0 +1,34 @@
+import CredentialsProvider from '@auth/core/providers/credentials'
+import { defineConfig } from 'auth-astro'
+
+export default defineConfig({
+  providers: [
+    CredentialsProvider({
+      // The name to display on the sign in form (e.g. "Sign in with...")
+      name: 'Credentials',
+      // `credentials` is used to generate a form on the sign in page.
+      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+      // e.g. domain, username, password, 2FA token, etc.
+      // You can pass any HTML attribute to the <input> tag through the object.
+      credentials: {
+        username: { label: 'Username', type: 'text', placeholder: 'codeseys' },
+        password: { label: 'Password', type: 'password' },
+      },
+      async authorize(credentials, req) {
+        if (
+          credentials.username === 'codeseys' &&
+          credentials.password === 'generalpass'
+        ) {
+          return {
+            id: '1',
+            name: 'codeseys',
+            email: 'anon@codeseys.io',
+          }
+        } else {
+          return null
+        }
+      },
+    }),
+  ],
+  secret: '...',
+})
