83d4e53fccae9a391532f7991388a5fd3d401651
Baladithya Balamurugan <bbaladithya@gmail.com>
2024-05-17 14:03:13 -0700

    moving middleware back and updateing hello api

--- src/pages/api/hello/[hello].ts
+++ src/pages/api/hello/[hello].ts
@@ -1,11 +1,12 @@
 // src/pages/api/hello/[name].ts
 import type { APIRoute } from 'astro';
 
-export const GET: APIRoute = ({ params }) => {
+export const GET: APIRoute = ({ params, request }) => {
+
     const { name } = params;
     const greeting = `Hello, ${name}!`;
 
-    return new Response(JSON.stringify({ greeting }), {
+    return new Response(JSON.stringify({ greeting, params }), {
         status: 200,
         headers: {
             'Content-Type': 'application/json',
