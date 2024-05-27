66796617fb257235629261ed7ef38cdbe9044005
Baladithya Balamurugan <bbaladithya@gmail.com>
2024-05-27 14:27:37 -0700

    remove middlware logging + edited theme initialization

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,21 +8,21 @@ export const onRequest = async (context: { request: { method: string; }; }, next
     }
 
     const response = await next();
-    const clonedResponse = response.clone();
-    const body = await clonedResponse.json();
+    // const clonedResponse = response.clone();
+    // const body = await clonedResponse.json();
 
-    const logresp = await fetch('https://crudcrud.com/api/c3af5bcbd0824e3494c3cf2cdc46c3c8/unicorns', {
-        method: 'POST',
-        body: JSON.stringify({
-            title: 'balatest',
-            body: body,
-        }),
-        headers: {
-            'Content-type': 'application/json; charset=UTF-8',
-        },
-    });
+    // const logresp = await fetch('https://crudcrud.com/api/c3af5bcbd0824e3494c3cf2cdc46c3c8/unicorns', {
+    //     method: 'POST',
+    //     body: JSON.stringify({
+    //         title: 'balatest',
+    //         body: body,
+    //     }),
+    //     headers: {
+    //         'Content-type': 'application/json; charset=UTF-8',
+    //     },
+    // });
 
-    console.log(logresp);
+    // console.log(logresp);
 
     const headers = new Headers(response.headers);
     headers.append('Access-Control-Allow-Origin', '*');
